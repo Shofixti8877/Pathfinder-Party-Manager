e714df51a8d33a1f5239c941e59037e125e579fa
Amit Reif <amitreif@gmail.com>
2018-07-04 10:25:50 +0300

    everything up to topNavigation

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -22,7 +22,16 @@ export const logout = () =>  dispatch => {
 };
 
 export const confirm = token => dispatch =>
-api.user.confirm(token).then(user =>{
+  api.user.confirm(token).then(user =>{
   localStorage.pathfinderJWT = user.token;
   dispatch(userLoggedIn(user));
-})
+});
+
+export const resetPasswordRequest = ({ email }) => () =>
+  api.user.resetPasswordRequest(email);
+
+export const validateToken = token => () =>
+  api.user.validateToken(token);
+
+export const resetPassword = data => () =>
+  api.user.resetPassword(data);
